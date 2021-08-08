52772556c4a6a7e1943292c5b34480b423526179
danie <danie@LAPTOP-LEINAD>
2021-08-08 13:03:49 +0700

    Add DemoWeb TS

---
+++ Test Suites/DemoWeb-TS.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoWeb-TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6ee83e4b-7742-4675-926f-69a204a6b6fa</testSuiteGuid>
+   <testCaseLink>
+      <guid>204efd6c-9fab-4ca6-b59f-4c27b278dd5c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoWeb-TC/01-Login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>279f940a-f49d-4f4b-80ca-bc57ac92ce34</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoWeb-TC/99-Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
